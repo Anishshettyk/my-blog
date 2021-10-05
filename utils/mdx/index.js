55bfc9634d3841c9d5816c5d48aec332334fcdd4
Anishshettyk <anishshettyk@gmail.com>
2021-10-05 20:24:59 +0530

    error fixed

--- utils/mdx/index.js
+++ utils/mdx/index.js
@@ -1,6 +1,5 @@
 import fs from "fs";
 import path from "path";
-import { renderToString } from "next-mdx-remote";
 
 //get's path to specific folder
 export const filePaths = (dataFolder = "Data", subFolder) => {
@@ -15,23 +14,3 @@ export const getAllFiles = (dataFolder, subFolder) =>
   fs
     .readdirSync(filePaths(dataFolder, subFolder))
     .filter((path) => /\.mdx?$/.test(path));
-
-export const getFileBySlug = async (type, slug) => {
-  const source = fs.readFileSync(
-    path.join(process.cwd(), type, `${slug}.mdx`),
-    "utf-8"
-  );
-  const { data, content } = matter(source);
-  const mdxSource = await renderToString(content, {
-    mdxOptions: {
-      remarkPlugins: [],
-      rehypePlugins: [],
-    },
-  });
-  return {
-    mdxSource,
-    frontMatter: {
-      ...data,
-    },
-  };
-};
